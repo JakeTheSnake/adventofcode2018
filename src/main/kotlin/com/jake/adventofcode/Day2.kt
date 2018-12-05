1ab9d1fb63577dc6e2b807038debe494eeb73195
Jakob Ström <jacobo.strm@gmail.com>
2018-12-05 20:44:56 +0100

    Day2 part 1

--- src/main/kotlin/com/jake/adventofcode/Day2.kt
+++ src/main/kotlin/com/jake/adventofcode/Day2.kt
@@ -2,13 +2,22 @@ package com.jake.adventofcode
 
 import java.io.File
 
-val input = File("input/day2").useLines { it }
+val input = File("input/day2").useLines { it.toList() }
+    .map { it.toList() }
 
 fun main() {
     println(part1())
 }
 
-private fun part1() {
+private fun part1() =
+    toListOfCharSizes(input).flatten().filter { it == 2 }.count() *
+    toListOfCharSizes(input).flatten().filter { it == 3 }.count()
 
-}
+private fun toListOfCharSizes(list: List<List<Char>>) = list
+    .map { it.groupBy { c -> c } } // Groups every equal item
+    .map { toUniqueValueCount(it) }
+
+private fun toUniqueValueCount(it: Map<Char, List<Char>>) =
+    it.values.map { it.size }.distinct()
+
 
