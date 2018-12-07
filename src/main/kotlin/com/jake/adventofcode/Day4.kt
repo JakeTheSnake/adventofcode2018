9784104b8c37f54e5e9a9ed53a3521cdffa40b7a
Jakob Ström <jacobo.strm@gmail.com>
2018-12-07 23:20:45 +0100

    Day4 part2

--- src/main/kotlin/com/jake/adventofcode/Day4.kt
+++ src/main/kotlin/com/jake/adventofcode/Day4.kt
@@ -5,37 +5,16 @@ import java.io.File
 private val input = File("input/day4").readLines().sorted()
 
 fun main() {
-    val entry = input.splitOn { s -> s.contains("Guard") }
-        .map { toShift(it) }
+    val guardMap = input.splitOn { s -> s.contains("Guard") }
+        .map { parseShift(it) }
         .groupBy { it.guardId }
         .mapValues { squash(it.value) }
-        .maxBy { amountOfSleep(it.value.timeTable) }
 
-    val max = entry!!.value.timeTable.max()
-    val minute = entry.value.timeTable.indexOf(max)
-    println(entry.key * minute)
-
+    println(part1(guardMap))
+    println(part2(guardMap))
 }
 
-fun amountOfSleep(timeTable: Array<Int>) = timeTable.reduce { acc, i -> acc + i }
-
-fun squash(v: List<Shift>) =
-    Shift(v[0].guardId,
-            v.map { it.timeTable }
-            .reduce { acc, shift -> mergeEachIndex(acc, shift) { a, b -> a + b } }
-)
-
-/**
- * Returns a new array by merging each index using the given transform-function
- */
-fun <T> mergeEachIndex(a1: Array<T>, a2: Array<T>, f: (T, T) -> T): Array<T> {
-    for (i in 0 until a1.size) {
-        a1[i] = f.invoke(a1[i], a2[i])
-    }
-    return a1
-}
-
-fun toShift(shift: List<String>): Shift {
+fun parseShift(shift: List<String>): Shift {
     val iterator = shift.iterator()
     val guardId = Regex("Guard #(\\d+)").find(iterator.next())!!.groupValues[1].toInt()
     val timeTable = Array(60) { 0 }
@@ -46,6 +25,46 @@ fun toShift(shift: List<String>): Shift {
             timeTable[i] = 1
     }
     return Shift(guardId, timeTable)
+}
+
+fun squash(v: List<Shift>) =
+    Shift(v[0].guardId,
+        v.map { it.timeTable }
+            .reduce { acc, shift -> mergeEachIndex(acc, shift) { a, b -> a + b } }
+    )
+
+private fun part1(guardMap: Map<Int, Shift>): Int {
+    val entry = guardMap.maxBy { amountOfSleep(it.value.timeTable) }
+    val max = entry!!.value.timeTable.max()
+    val minute = entry.value.timeTable.indexOf(max)
+    return entry.key * minute
+}
+
+
+private fun part2(guardMap: Map<Int, Shift>): Int {
+    val entry = guardMap.maxBy { it.value.timeTable.max().intOr(0) }
+    val max = entry!!.value.timeTable.max()
+    val minute = entry.value.timeTable.indexOf(max)
+    return entry.key * minute
+}
+
+private fun Int?.intOr(i: Int): Int {
+    if (this == null) {
+        return i
+    }
+    return this
+}
+
+fun amountOfSleep(timeTable: Array<Int>) = timeTable.reduce { acc, i -> acc + i }
+
+/**
+ * Returns a new array by merging each index using the given transform-function
+ */
+fun <T> mergeEachIndex(a1: Array<T>, a2: Array<T>, f: (T, T) -> T): Array<T> {
+    for (i in 0 until a1.size) {
+        a1[i] = f.invoke(a1[i], a2[i])
+    }
+    return a1
 }
 
 fun getMinute(line: String) = Regex("\\d{2}:(\\d{2})").find(line)!!.groupValues[1].toInt()
@@ -71,5 +90,4 @@ fun <T> Iterable<T>.splitOn(t: (T) -> Boolean): List<List<T>> {
         result.add(chunk)
     }
     return result
-
 }
